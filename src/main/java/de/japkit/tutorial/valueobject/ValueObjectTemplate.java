2a8e8c43a6563039af807603f381baf6395fed0d
Stefan Ocke <stefanocke@users.noreply.github.com>
2017-02-12 14:11:25 +0100

    Step 6 - Generate getters and private setters
    Generate getters and private setters for the field. The setters will be
    used later by the generated builder class.
    You can generate arbitrary methods with japkit, but getters and setters
    are so common, that there are convenient @Getter and @Setter annotations
    to generate the accessor methods for a field.
    
    We also tell japkit to generate an implements clause here for the
    SrcIterface which is IPerson in our example.
    
    In the generated class, the @ParamNames annotations have a similar
    purpose as @Order. They provide method parameter names for cases where
    only binary type information is provided to the annotation processor.
    You can simply ignore it.

--- src/main/java/de/japkit/tutorial/valueobject/ValueObjectTemplate.java
+++ src/main/java/de/japkit/tutorial/valueobject/ValueObjectTemplate.java
@@ -1,11 +1,18 @@
 package de.japkit.tutorial.valueobject;
 
+import javax.lang.model.element.Modifier;
+
+import de.japkit.functions.SrcInterface;
 import de.japkit.functions.SrcType;
 import de.japkit.metaannotations.Clazz;
 import de.japkit.metaannotations.Field;
+import de.japkit.metaannotations.Getter;
+import de.japkit.metaannotations.Setter;
 
 @Clazz(namePrefixToRemove="I", nameSuffixToAppend="")
-public class ValueObjectTemplate {
-	@Field(src = "#{src.properties}")
+public class ValueObjectTemplate implements SrcInterface {
+	@Field(src = "#{src.properties}",
+			getter = @Getter,
+			setter = @Setter(modifiers=Modifier.PRIVATE))
 	private SrcType $name$;
 }
