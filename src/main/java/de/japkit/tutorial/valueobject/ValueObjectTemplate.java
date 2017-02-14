7a557bcd676874bf272ad2a814e757e2e1ddaa63
Stefan Ocke <stefanocke@users.noreply.github.com>
2017-02-14 18:16:15 +0100

    Step 9 - The build method
    At first, remember the type of the generates value object class
    (Person). Then, generate the build method. It's code body creates an
    instance of the value object and then sets each property of the value
    object by calling the private setters. Finally, the value object is
    returned. 
    Note, that "src" can be omitted in most places, for example "properties"
    instead of "src.properties".
    The "code" in "valueObjectClass.code" takes care for proper
    representation of the qualified type name in the generated code. If
    possible, an import statement is added.

--- src/main/java/de/japkit/tutorial/valueobject/ValueObjectTemplate.java
+++ src/main/java/de/japkit/tutorial/valueobject/ValueObjectTemplate.java
@@ -8,7 +8,10 @@ import de.japkit.metaannotations.Clazz;
 import de.japkit.metaannotations.Field;
 import de.japkit.metaannotations.Getter;
 import de.japkit.metaannotations.InnerClass;
+import de.japkit.metaannotations.Method;
 import de.japkit.metaannotations.Setter;
+import de.japkit.metaannotations.Var;
+import de.japkit.metaannotations.classselectors.GeneratedClass;
 
 @Clazz(namePrefixToRemove="I", nameSuffixToAppend="")
 public class ValueObjectTemplate implements SrcInterface {
@@ -16,13 +19,27 @@ public class ValueObjectTemplate implements SrcInterface {
 			getter = @Getter,
 			setter = @Setter(modifiers=Modifier.PRIVATE))
 	private SrcType $name$;
-	
+
+	@Var(fun = GeneratedClass.class)
+	class ValueObjectClass {
+	}
+
 	@InnerClass
 	static class Builder {
 		@Field(src = "#{src.properties}",
 				getter = @Getter,
 				setter = @Setter(chain=true))
 		private SrcType $name$;
-		
+
+		@Method(
+				bodyIterator = "#{properties}",
+				bodyBeforeIteratorCode = "#{valueObjectClass.code} vo = new #{valueObjectClass.code}();",
+				bodyCode = "vo.#{setterName}(#{name});",
+				bodyAfterIteratorCode = "return vo;",
+				bodyLinebreak = true)
+		public ValueObjectClass build() {
+			return null;
+		}
+
 	}
 }
