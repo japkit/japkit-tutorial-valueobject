8ba282e601fb63b6f6a9b4ed7a4e378e196e759b
Stefan Ocke <stefanocke@users.noreply.github.com>
2017-02-26 16:54:09 +0100

    Step 14.3 - JPA annotations: More Matcher functions
    As preparation for the next steps, we introduce a bunch of new Matcher
    functions to distinguish between several types of fields in a value
    object. 
    There are some new concepts here:
    - Boolean functions (like Matchers) can be combined by @And, @Or, @Not.
    - japkit provides a convenient property "singleValueType" to determine
    the element type of a collection (like Set or List). Similar, Matchers
    can operate on the singleValueType when matching for annotations
    ("singleValueTypeAnnotations"). 
    - "asElement().ValueObject" shows how to easily access an elements
    annotation by its simple name (ValueObject). To make this work, you have
    to add a so called annotation import to the library.

--- src/main/java/de/japkit/tutorial/valueobject/DomainLibrary.java
+++ src/main/java/de/japkit/tutorial/valueobject/DomainLibrary.java
@@ -1,15 +1,29 @@
 package de.japkit.tutorial.valueobject;
 
+import static de.japkit.metaannotations.TypeCategory.ENUM;
+import static de.japkit.metaannotations.TypeCategory.MATH;
+import static de.japkit.metaannotations.TypeCategory.PRIMITIVE;
+import static de.japkit.metaannotations.TypeCategory.STRING;
+import static de.japkit.metaannotations.TypeCategory.TEMPORAL;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+import de.japkit.metaannotations.And;
+import de.japkit.metaannotations.Library;
 import de.japkit.metaannotations.Matcher;
+import de.japkit.metaannotations.Not;
+import de.japkit.metaannotations.Or;
 
 /**
  * Common functions for code templates for domain classes.
  *
  */
+@Library(annotationImports = ValueObject.class)
 public class DomainLibrary {
 	@Matcher(type = Date.class)
 	public @interface isDate {
@@ -21,6 +35,68 @@ public class DomainLibrary {
 
 	@Matcher(type = Set.class)
 	public @interface isSet {
+	}
+	
+	@Matcher(singleValueTypeAnnotations = Entity.class)
+	public @interface isJpaEntity {
+	}
+
+	@Matcher(singleValueTypeAnnotations = Embeddable.class)
+	public @interface isJpaEmbeddable {
+	}
+
+	@Matcher(singleValueTypeAnnotations = ValueObject.class)
+	public @interface isValueObject {
+	}
+
+	@Matcher(singleValueTypeAnnotations = ValueObject.class, condition = "#{singleValueType.asElement().ValueObject.asJpaEntity == true}")
+	public @interface isValueObjectAsJpaEntity {
+	}
+
+	@Matcher(singleValueTypeAnnotations = ValueObject.class, condition = "#{singleValueType.asElement().ValueObject.asJpaEntity != true}")
+	public @interface isValueObjectAsEmbeddable {
+	}
+
+	/**
+	 * JPA Basic type
+	 */
+	@Matcher(singleValueTypeCategory = { PRIMITIVE, MATH, STRING, TEMPORAL, ENUM })
+	public @interface isJpaBasic {
+	}
+
+	@Matcher(singleValueTypeCategory = { ENUM })
+	public @interface isEnum {
+	}
+
+	@Or({ isJpaBasic.class, isJpaEmbeddable.class, isValueObjectAsEmbeddable.class })
+	public @interface isJpaBasicOrEmbeddable {
+	}
+
+	@Or({ isJpaEmbeddable.class, isValueObjectAsEmbeddable.class })
+	public @interface isEmbeddable {
+	}
+
+	/**
+	 * @ValueObject or hand-written @Embeddable.
+	 */
+	@Or({ isValueObject.class, isJpaEmbeddable.class })
+	public @interface isValueObjectOrJpaEmbeddable {
+	}
+
+	@Not(isValueObject.class)
+	public @interface isNotValueObject {
+	}
+
+	@And({ isJpaEntity.class, isNotValueObject.class })
+	public @interface isEntity {
+	}
+
+	@Or({ isList.class, isSet.class })
+	public @interface isMultiValued {
+	}
+
+	@Not(isMultiValued.class)
+	public @interface isSingleValued {
 	}
 
 }
